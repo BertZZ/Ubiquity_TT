f2b9922ef87bc2e008112df3eef0794cbeef54fb
BertZZ <bertie.wooles@gmail.com>
2020-02-05 16:24:59 +0000

    Completes user regestration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,6 @@ describe User do
   it { should validate_uniqueness_of(:email)}
 
   it 'does not store the password as plain text' do
-    puts subject.password_digest
     expect(subject.password_digest).not_to eq('Potato123!')
   end
 end
